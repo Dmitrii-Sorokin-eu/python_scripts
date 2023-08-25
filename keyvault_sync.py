b986f4577f5bd061bc336052be110405390ad091
Dmitrii-Sorokin-eu <113148176+Dmitrii-Sorokin-eu@users.noreply.github.com>
2023-08-25 21:26:06 +0200

    Update keyvault_sync.py

--- keyvault_sync.py
+++ keyvault_sync.py
@@ -1,7 +1,7 @@
 import argparse
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
-from tqdm import tqdm  # Import the tqdm library for the progress bar
+from tqdm import tqdm
 
 def sync_secrets(source_kv_uri, target_kv_uris, replace_if_exist):
     source_credential = DefaultAzureCredential()
@@ -23,16 +23,34 @@ def sync_secrets(source_kv_uri, target_kv_uris, replace_if_exist):
             else:
                 target_client.set_secret(secret_name, secret_value)
                 tqdm.write(f"Secret '{secret_name}' successfully synchronized to the target Key Vault.")
+
+def list_keyvaults():
+    credential = DefaultAzureCredential()
+    secret_client = SecretClient(vault_url="https://<your-keyvault-name>.vault.azure.net/", credential=credential)
+    keyvaults = secret_client.list_properties_of_secrets()
+    
+    for keyvault in keyvaults:
+        print(keyvault.id)
             
 def main():
-    parser = argparse.ArgumentParser(description="Synchronize secrets between Azure Key Vaults.")
-    parser.add_argument("--source-keyvault", required=True, help="Source Key Vault URI")
-    parser.add_argument("--target-keyvaults", required=True, nargs="+", help="Target Key Vault URIs (separated by space)")
-    parser.add_argument("--replace-if-exist", action="store_true", help="Replace secrets if they already exist in target Key Vaults")
-    
+    parser = argparse.ArgumentParser(description="Synchronize secrets between Azure Key Vaults or list available Key Vaults.")
+    subparsers = parser.add_subparsers(dest="command")
+
+    parser_sync = subparsers.add_parser("sync", help="Synchronize secrets between Key Vaults")
+    parser_sync.add_argument("--source-keyvault", required=True, help="Source Key Vault URI")
+    parser_sync.add_argument("--target-keyvaults", required=True, nargs="+", help="Target Key Vault URIs (separated by space)")
+    parser_sync.add_argument("--replace-if-exist", action="store_true", help="Replace secrets if they already exist in target Key Vaults")
+
+    parser_list = subparsers.add_parser("list-keyvaults", help="List available Key Vaults")
+
     args = parser.parse_args()
-    
-    sync_secrets(args.source_keyvault, args.target_keyvaults, args.replace_if_exist)
-    
+
+    if args.command == "sync":
+        sync_secrets(args.source_keyvault, args.target_keyvaults, args.replace_if_exist)
+    elif args.command == "list-keyvaults":
+        list_keyvaults()
+    else:
+        print("Invalid command. Use 'sync' or 'list-keyvaults'.")
+
 if __name__ == "__main__":
     main()
